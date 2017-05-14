76834367b0f752921f8a5d3ac1cd90dc5d5befa4
CarterMoore <cartermoore8@gmail.com>
2017-05-14 19:11:33 -0400

    Updated Comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -14,13 +14,13 @@ public class TicTacToe extends Applet implements MouseListener{
     
     private int size = 3; // Dimensions of the board will be able to change
     private int board[][] = new int[size][size]; // Store an int on what is in the square, -1 = O, 0 = blank, 1 = X
-    private final int LENGTH = 600; // Size of the window
-    private boolean isPvp = false; // Whether or not game is player vs player
+    private final int LENGTH = 600; // Size of the window (600 x 600)
+    private boolean isPvp = true; // Whether or not game is player vs player
     private int player = 1; // Which player has their move (X starts)
-    private int difficulty = 2;
+    private int difficulty = 2; // Difficulty of bot. 1 = Easy, 2 = Medium, 3 = Impossible
     
     public void init() {
-        addMouseListener(this);
+        addMouseListener(this); // Create mouse listener
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 board[i][j] = 0; // Set all board spaces as 0
@@ -29,15 +29,16 @@ public class TicTacToe extends Applet implements MouseListener{
     }
     
     public void paint(Graphics g) {
-                for (int i = 1; i < size; i++) { // Draw lines depending on given size
-                g.fillRect(i * (LENGTH / size) - 2, 0, 4, LENGTH);
-                g.fillRect(0, i * (LENGTH / size) - 2, LENGTH, 4);
+        
+        for (int i = 1; i < size; i++) { // Draw lines depending on given size
+            g.fillRect(i * (LENGTH / size) - 2, 0, 4, LENGTH);
+            g.fillRect(0, i * (LENGTH / size) - 2, LENGTH, 4);
         }
         
-        Image xImage = getImage(getClass().getResource("X.png"));
-        Image oImage = getImage(getClass().getResource("O.png"));
+        Image xImage = getImage(getClass().getResource("X.png")); // Get X image
+        Image oImage = getImage(getClass().getResource("O.png")); // Get O image
         
-        // Loop though and draw X or O depending on state of board space
+        // Loop though and draw X or O depending on state of board
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                 if (board[i][j] == 1)
@@ -49,8 +50,10 @@ public class TicTacToe extends Applet implements MouseListener{
     }
     
     private int getWinner() {
+        
         int winner = 0;
         
+        // Set all win possibilities as true
         boolean diagonalW1 = true, diagonalW2 = true;
             for (int i = 0; i < size; i++) {
             boolean horizontalW = true, verticalW = true;
@@ -76,10 +79,9 @@ public class TicTacToe extends Applet implements MouseListener{
         
         if (diagonalW1 && board[0][0] != 0) // Make sure a player won
             winner = board[0][0];
-            if (diagonalW2 && board[0][size-1] != 0) // Make sure a player won
-                winner = board[0][size-1];
+        if (diagonalW2 && board[0][size-1] != 0) // Make sure a player won
+            winner = board[0][size-1];
             
-        System.out.println(winner);
         return winner; // Return who won, -1 = O, 0 = nobody, 1 = X
     }
     
@@ -93,7 +95,8 @@ public class TicTacToe extends Applet implements MouseListener{
                 int y = random.nextInt(size);
             
             // Randomize until available space is found
-            while (board[x][y] != 0) {
+            // Make sure a move can be made. Will be caught in infinite loop if not checked
+            while (availableSpot() && board[x][y] != 0) {
                     x = random.nextInt(size);
                     y = random.nextInt(size);
             }
@@ -133,8 +136,21 @@ public class TicTacToe extends Applet implements MouseListener{
             
             botMove(1);
             
+        }else { // Impossible difficulty, uses minimax algorithm
+            
         }
         
+    }
+    
+    private boolean availableSpot () {
+        // Check if there are any moves left to make
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == 0)
+                    return true;
+            }
+        }
+        return false;
     }
     
     public void mouseClicked (MouseEvent e) {
@@ -143,17 +159,23 @@ public class TicTacToe extends Applet implements MouseListener{
         int x = e.getX();
         int y = e.getY();
         
+        // Calculate which squares are clicked from the coordinates
+        // e.g. x = 100, 100 / (600 / 3) = 0
         int moveX = x / (LENGTH / size);
         int moveY = y / (LENGTH / size);
         
+        // If the game is two player, swap between X and O every turn
         if (board[moveX][moveY] == 0 && getWinner() == 0 && isPvp) {
             board[moveX][moveY] = player;
             player *= -1;
-        }else if (board[moveX][moveY] == 0 && getWinner() == 0) {
+        }
+        // If the game is single player call the botMove procedure after every turn
+        else if (board[moveX][moveY] == 0 && getWinner() == 0) {
             board[moveX][moveY] = 1;
             botMove(difficulty);
         }
         
+        // Draw the updated board
         repaint();
     }
     
